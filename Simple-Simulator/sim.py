6daf9c5b293a1d590d2d4b2f16986187dd76676d
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2022-07-26 15:22:37 +0530

    Update sim.py

--- Simple-Simulator/sim.py
+++ Simple-Simulator/sim.py
@@ -22,6 +22,17 @@ opcodeType = {
     "01010":"F",
 }
 
+registers = {
+	'R0':'000',
+	'R1':'001',
+	'R2':'010',
+	'R3':'011',
+	'R4':'100',
+	'R5':'101',
+	'R6':'110',
+	'FLAGS':'111'
+}
+
 inp = []
 instructions = []
 pc = 0 #Program Counter
